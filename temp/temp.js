6d9368afaa9f30e7884386d7fbfcf54b429c2807
Romesh Jain <romeshjainofficial@gmail.com>
2026-03-12 11:50:53 +0530

    feat: update .gitignore and add initial job script and configuration files

---
+++ temp/temp.js
@@ -0,0 +1,20 @@
+const URLS = [
+  {
+    type: 'react',
+    keywords:
+      'react.js, react js developer, react js frontend developer, react developer, nextjs, typescript, javascript',
+    url: 'https://www.naukri.com/react-dot-js-react-js-developer-react-js-frontend-developer-react-developer-nextjs-typescript-javascript-jobs?k=react.js%2C%20react%20js%20developer%2C%20react%20js%20frontend%20developer%2C%20react%20developer%2C%20nextjs%2C%20typescript%2C%20javascript&experience=3&nignbevent_src=jobsearchDeskGNB',
+  },
+  {
+    type: 'mern',
+    keywords:
+      'mern stack, mern stack developer, mern full stack developer, frontend development, frontend software developer, node.js, backend, mern stack, mern, express, nestjs',
+    url: 'https://www.naukri.com/mern-stack-mern-stack-developer-mern-full-stack-developer-frontend-development-frontend-software-developer-node-dot-js-backend-mern-stack-mern-express-nestjs-jobs?k=mern%20stack%2C%20mern%20stack%20developer%2C%20mern%20full%20stack%20developer%2C%20frontend%20development%2C%20frontend%20software%20developer%2C%20node.js%2C%20backend%2C%20mern%20stack%2C%20mern%2C%20express%2C%20nestjs&experience=3&nignbevent_src=jobsearchDeskGNB',
+  },
+  {
+    type: 'reactnative-node',
+    keywords:
+      'React Native, React Native Developer, React Native Mobile Application Developer, Node.js, Node Js Backend Developer, Node Js Developer, Backend Development, Backend, React Native',
+    url: 'https://www.naukri.com/react-native-react-native-developer-react-native-mobile-application-developer-node-dot-js-node-js-backend-developer-node-js-developer-backend-development-backend-react-native-jobs?k=react%20native%2C%20react%20native%20developer%2C%20react%20native%20mobile%20application%20developer%2C%20node.js%2C%20node%20js%20backend%20developer%2C%20node%20js%20developer%2C%20backend%20development%2C%20backend%2C%20react%20native&experience=3&nignbevent_src=jobsearchDeskGNB',
+  },
+];

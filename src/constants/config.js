caef85234e02163d04dcaf9d43d0cdcde2caf6ee
Romesh Jain <romeshjainofficial@gmail.com>
2026-03-26 22:25:40 +0530

    updated config

--- src/constants/config.js
+++ src/constants/config.js
@@ -16,7 +16,7 @@ export const CONFIG = {
   },
 
   BROWSER: {
-    headless: false,
+    headless: true,
     slowMo: 80,
     userAgent:
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36',
